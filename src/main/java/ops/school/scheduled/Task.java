bdeb5d6e88c79bf7ebf0ce28c6087dc28bd77a62
leezhen <2371601496@qq,com>
2019-07-13 11:45:09 +0800

    000000

--- src/main/java/ops/school/scheduled/Task.java
+++ src/main/java/ops/school/scheduled/Task.java
@@ -4,12 +4,13 @@ package ops.school.scheduled;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.github.qcloudsms.httpclient.HTTPException;
-import com.sun.xml.internal.bind.v2.TODO;
-import ops.school.api.dao.*;
 import ops.school.api.dto.RunOrdersTj;
 import ops.school.api.entity.*;
 import ops.school.api.service.*;
-import ops.school.api.util.*;
+import ops.school.api.util.LoggerUtil;
+import ops.school.api.util.RedisUtil;
+import ops.school.api.util.SpringUtil;
+import ops.school.api.util.Util;
 import ops.school.controller.SignController;
 import ops.school.util.TimeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,8 +44,8 @@ public class Task {
     //0 0 10,14,16 * * ?   每天上午10点，下午2点，4点
     @Scheduled(cron = "0 0 2 * * ?")
     public void task() {
-        ordersService.remove(new QueryWrapper<Orders>().lambda().eq(Orders::getStatus,"待付款"));
-        runOrdersService.remove(new QueryWrapper<RunOrders>().lambda().eq(RunOrders::getStatus,"待付款"));
+        ordersService.remove(new QueryWrapper<Orders>().lambda().eq(Orders::getStatus, "待付款"));
+        runOrdersService.remove(new QueryWrapper<RunOrders>().lambda().eq(RunOrders::getStatus, "待付款"));
     }
 
     @Scheduled(cron = "0 0 0 * * ?")
@@ -68,9 +69,9 @@ public class Task {
         String day = getYesterdayByCalendar();
         Map<String, Object> map = new HashMap<>();
         map.put("day", day + "%");
-        List<School> schools = schoolService.list(new QueryWrapper<School>().lambda().eq(School::getIsDelete,0));
+        List<School> schools = schoolService.list(new QueryWrapper<School>().lambda().eq(School::getIsDelete, 0));
         for (School schooltemp : schools) {
-            List<Shop> shops = shopService.list(new QueryWrapper<Shop>().lambda().eq(Shop::getSchoolId,schooltemp.getId()));
+            List<Shop> shops = shopService.list(new QueryWrapper<Shop>().lambda().eq(Shop::getSchoolId, schooltemp.getId()));
             for (Shop shoptemp : shops) {
                 map.put("shopId", shoptemp.getId());
                 Orders result = ordersService.completeByShopId(map);
@@ -100,104 +101,76 @@ public class Task {
 
 
     /**
-     * 每5分钟执行一次
+     * 每1分钟执行一次,隔5分钟提醒一次
      * 提醒学校负责人，商家有超时未接手订单
      */
-    @Scheduled(cron = "0 */5 * * * ?")
+    @Scheduled(cron = "0 */1 * * * ?")
     public void remindUntakenOrders() {
 
         // 查询所有待接手订单
         List<Orders> ordersList;
-        if (SpringUtil.redisCache()){
+        if (SpringUtil.redisCache()) {
             ordersList = JSON.parseArray(stringRedisTemplate.boundHashOps("ALL_DJS").values().toString(), Orders.class);
-        }else{
-            ordersList=ordersService.findAllDjs();
+        } else {
+            ordersList = ordersService.findAllDjs();
         }
         //TODO
         //服务重启的话缓存会漏单
-        for(Orders temp : ordersList){
+        for (Orders temp : ordersList) {
             Shop shop = shopService.getById(temp.getShopId());
-            if(stringRedisTemplate.opsForValue().get("SCHOOL_NOTIFY_SHOP"+shop.getShopPhone())!=null){
-                try {
-                    Util.qqsms(1400169549, "0eb188f83ef4b2dc8976b5e76c70581e", shop.getShopPhone(), 372755, shop.getShopName()+","+shop.getShopPhone(), null);
-                } catch (Exception e) {
-                    LoggerUtil.log(e.getMessage());
+            School school = schoolService.findById(temp.getSchoolId());
+            // 获取当前时间
+            long current = System.currentTimeMillis();
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String curTime = df.format(current);
+            // 获取订单支付时间
+            long differTime = TimeUtil.dateDiff(temp.getPayTime(), curTime);
+            if (differTime > 10) {
+                if (stringRedisTemplate.opsForValue().get("SCHOOL_NOTIFY_SHOP" + school.getPhone()) == null) {
+                    try {
+                        Util.qqsms(1400169549, "0eb188f83ef4b2dc8976b5e76c70581e", school.getPhone(), 372755, shop.getShopName() + "," + school.getPhone(), null);
+                    } catch (Exception e) {
+                        LoggerUtil.log(e.getMessage());
+                    }
+                    stringRedisTemplate.opsForValue().set("SCHOOL_NOTIFY_SHOP" + school.getPhone(), "", 5, TimeUnit.MINUTES);
                 }
-                stringRedisTemplate.opsForValue().set("SCHOOL_NOTIFY_SHOP"+shop.getShopPhone(),"",10,TimeUnit.MINUTES);
             }
         }
-       /* List<School> schoolList = schoolService.find(new School());
-            for (School sch: schoolList) {
-                int count = 0;
-                if (ordersList.size()>0) {
-                    for (int i = 0;i < ordersList.size();i++) {
-                        if (sch.getId().equals(ordersList.get(i).getSchoolId())){
-                            long current = System.currentTimeMillis();
-                            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                            String curTime = df.format(current);
-                            // 获取订单支付时间
-                            long differTime = TimeUtil.dateDiff(ordersList.get(0).getPayTime(), curTime);
-                            if (differTime > 10) {
-                                count++;
-                                if (count != 0 && i == (ordersList.size()-1)){
-                                    String params = count +"";
-                                    try {
-                                        Util.qqsms(1400169549, "0eb188f83ef4b2dc8976b5e76c70581e", sch.getPhone(), 372252, params, null);
-                                    } catch (HTTPException | IOException | org.json.JSONException e) {
-                                        LoggerUtil.log(e.getMessage());
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }*/
     }
 
     /**
-     * 每5分钟执行一次
+     * 每1分钟执行一次，隔5分钟提醒一次
      * 提醒学校负责人，配送员有超时未接手订单
      */
-    @Scheduled(cron = "0 */5 * * * ?")
+    @Scheduled(cron = "0 */1 * * * ?")
     public void remindDistributor() {
 
         // 查询所有商家已接手订单
         List<Orders> orders;
-        if (SpringUtil.redisCache()){
+        if (SpringUtil.redisCache()) {
             orders = JSON.parseArray(stringRedisTemplate.boundHashOps("SHOP_YJS").values().toString(), Orders.class);
-        }else{
+        } else {
             Orders orders1 = new Orders();
             orders1.setStatus("商家已接手");
-            orders=ordersService.find(orders1);
+            orders = ordersService.find(orders1);
         }
-        System.out.println("商家已接手："+orders.size());
-        List<School> schoolList = schoolService.find(new School());
-        if (!schoolList.isEmpty()){
-            for (School sch: schoolList) {
-                int count = 0;
-                if (!orders.isEmpty()) {
-                    for (int i = 0;i < orders.size();i++) {
-                        if (sch.getId().equals(orders.get(i).getSchoolId())){
-                            long current = System.currentTimeMillis();
-                            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                            String curTime = df.format(current);
-                            System.out.println("商家接手时间：" + orders.get(0).getShopAcceptTime());
-                            // 获取订单支付时间
-                            long differTime = TimeUtil.dateDiff(orders.get(0).getShopAcceptTime(), curTime);
-                            System.out.println(differTime);
-                            if (differTime > 10) {
-                                count++;
-                                if (count != 0 && i == (orders.size()-1)){
-                                    String params = count +"";
-                                    try {
-                                        Util.qqsms(1400169549, "0eb188f83ef4b2dc8976b5e76c70581e", sch.getPhone(), 372253, params, null);
-                                        stringRedisTemplate.opsForValue().set(sch.getPhone(), params, 5, TimeUnit.MINUTES);
-                                    } catch (HTTPException | IOException | org.json.JSONException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }
+        if (!orders.isEmpty()) {
+            for (Orders order : orders) {
+                School school = schoolService.findById(order.getSchoolId());
+                long currentTime = System.currentTimeMillis();
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String curTime = df.format(currentTime);
+                // 获取商家接手时间
+                long differTime = TimeUtil.dateDiff(orders.get(0).getShopAcceptTime(), curTime);
+                if (differTime > 10) {
+                    if (stringRedisTemplate.opsForValue().get("SCHOOL_NOTIFY_SENDER" + school.getPhone()) == null) {
+                        try {
+                            Util.qqsms(1400169549, "0eb188f83ef4b2dc8976b5e76c70581e", school.getPhone(), 372793, "", null);
+                            stringRedisTemplate.opsForValue().set(school.getPhone(), "", 5, TimeUnit.MINUTES);
+                        } catch (HTTPException | IOException | org.json.JSONException e) {
+                            e.printStackTrace();
                         }
+                        stringRedisTemplate.opsForValue().set("SCHOOL_NOTIFY_SENDER" + school.getPhone(), "", 5, TimeUnit.MINUTES);
                     }
                 }
             }
--- src/main/java/ops/school/util/TimeUtil.java
+++ src/main/java/ops/school/util/TimeUtil.java
@@ -12,21 +12,12 @@ public class TimeUtil {
         long nd = 1000 * 24 * 60 * 60;// 一天的毫秒数
         long nh = 1000 * 60 * 60;// 一小时的毫秒数
         long nm = 1000 * 60;// 一分钟的毫秒数
-        long ns = 1000;// 一秒钟的毫秒数
         long diff = 2L;
         long day = 0;
-        long hour = 0;
         long min = 0;
-        long sec = 0;
-        // 获得两个时间的毫秒时间差异
-        System.out.println(startTime+ "------" +endTime);
         try {
             diff = sd.parse(endTime).getTime() - sd.parse(startTime).getTime();
-            day = diff / nd;// 计算差多少天
-//            hour = diff % nd / nh + day * 24;// 计算差多少小时
             min = diff % nd % nh / nm + day * 24 * 60;// 计算差多少分钟
-//            sec = diff % nd % nh % nm / ns;// 计算差多少秒
-
         } catch (ParseException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
