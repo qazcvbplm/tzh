1038967c1a1faf6d901c0c54a884b70b21385983
Fang <fangfor@yeah.net>
2019-07-15 19:38:15 +0800

    aaa

---
+++ src/main/java/ops/school/constants/OrderContants.java
@@ -0,0 +1,18 @@
+package ops.school.constants;
+
+/**
+ * CreatebyFang
+ * fangfor@outlook.com
+ * 2019/7/15
+ * 19:04
+ * #
+ */
+public class OrderContants {
+
+    public final static String orderTypeTakeOut = "外卖订单";
+
+    public final static String orderTypeRunning = "外卖订单";
+
+
+
+}
