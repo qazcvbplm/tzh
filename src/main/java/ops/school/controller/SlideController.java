3860cae3d87a133555249f9b72631755fe884a2c
tong'zhu'he <tongzhuhe@lidig.com>
2019-07-15 22:14:11 +0800

    轮播图排序

--- src/main/java/ops/school/controller/SlideController.java
+++ src/main/java/ops/school/controller/SlideController.java
@@ -23,49 +23,51 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@Api(tags="轮播图模块")
+@Api(tags = "轮播图模块")
 @RequestMapping("ops/slide")
 public class SlideController {
 
-	@Autowired
-	private SlideService slideService;
-	@Autowired
-	private StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    private SlideService slideService;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
-	@ApiOperation(value="添加",httpMethod="POST")
-	@PostMapping("add")
-	public ResponseObject add(HttpServletRequest request, HttpServletResponse response, @ModelAttribute @Valid Slide slide, BindingResult result){
-		              Util.checkParams(result);
+    @ApiOperation(value = "添加", httpMethod = "POST")
+    @PostMapping("add")
+    public ResponseObject add(HttpServletRequest request, HttpServletResponse response, @ModelAttribute @Valid Slide slide, BindingResult result) {
+        Util.checkParams(result);
         if (SpringUtil.redisCache()) {
-		            	  stringRedisTemplate.boundHashOps("SLIDE_LIST").delete(slide.getSchoolId()+"");
-		              }
-		slideService.save(slide);
-		              return new ResponseObject(true, "添加成功");
-	}
-	
-	@ApiOperation(value="查询",httpMethod="POST")
-	@PostMapping("find")
-	public ResponseObject find(HttpServletRequest request,HttpServletResponse response,int schoolId){
-						List<Slide> list=null;
+            stringRedisTemplate.boundHashOps("SLIDE_LIST").delete(slide.getSchoolId() + "");
+        }
+        slideService.save(slide);
+        return new ResponseObject(true, "添加成功");
+    }
+
+    @ApiOperation(value = "查询", httpMethod = "POST")
+    @PostMapping("find")
+    public ResponseObject find(HttpServletRequest request, HttpServletResponse response, int schoolId) {
+        List<Slide> list = null;
         if (SpringUtil.redisCache()) {
-							if(stringRedisTemplate.boundHashOps("SLIDE_LIST").get(schoolId+"")!=null){
-								 list=JSON.parseArray(stringRedisTemplate.boundHashOps("SLIDE_LIST").get(schoolId+"").toString(), Slide.class);
-							}else{
-								list = slideService.findBySchoolId(schoolId);
-								stringRedisTemplate.boundHashOps("SLIDE_LIST").put(schoolId+"", JSON.toJSONString(list));
-							}
-						}
-		              return new ResponseObject(true, "ok").push("list", list);
-	}
-	
-	@ApiOperation(value="更新",httpMethod="POST")
-	@PostMapping("update")
-	public ResponseObject update(HttpServletRequest request,HttpServletResponse response,Slide slide){
-		boolean rs = slideService.updateById(slide);
-		if (SpringUtil.redisCache()) {
-			Slide temp = slideService.getById(slide.getId());
-		            	  stringRedisTemplate.boundHashOps("SLIDE_LIST").delete(temp.getSchoolId()+"");
-		              }
-		return new ResponseObject(true, "更新成功");
-	}
+            if (stringRedisTemplate.boundHashOps("SLIDE_LIST").get(schoolId + "") != null) {
+                list = JSON.parseArray(stringRedisTemplate.boundHashOps("SLIDE_LIST").get(schoolId + "").toString(), Slide.class);
+            } else {
+                QueryWrapper<Slide> query = new QueryWrapper<Slide>();
+                query.lambda().eq(Slide::getSchoolId, schoolId).orderByDesc(Slide::getSort);
+                list = slideService.list(query);
+                stringRedisTemplate.boundHashOps("SLIDE_LIST").put(schoolId + "", JSON.toJSONString(list));
+            }
+        }
+        return new ResponseObject(true, "ok").push("list", list);
+    }
+
+    @ApiOperation(value = "更新", httpMethod = "POST")
+    @PostMapping("update")
+    public ResponseObject update(HttpServletRequest request, HttpServletResponse response, Slide slide) {
+        boolean rs = slideService.updateById(slide);
+        if (SpringUtil.redisCache()) {
+            Slide temp = slideService.getById(slide.getId());
+            stringRedisTemplate.boundHashOps("SLIDE_LIST").delete(temp.getSchoolId() + "");
+        }
+        return new ResponseObject(true, "更新成功");
+    }
 }
