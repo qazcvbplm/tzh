a3e8c2c0396b328140074fac9c24888a7a4a7dfc
Fang <fangfor@yeah.net>
2019-07-17 17:43:57 +0800

    mapper生成07171701

---
+++ tzh-api/src/main/java/ops/school/api/dao/ShopFullCutMapper.java
@@ -0,0 +1,13 @@
+package ops.school.api.dao;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import ops.school.api.entity.ShopFullCut;
+import org.springframework.stereotype.Repository;
+
+/**
+ * ShopFullCutDAO继承基类
+ */
+@Repository
+public interface ShopFullCutMapper extends BaseMapper<ShopFullCut> {
+}
