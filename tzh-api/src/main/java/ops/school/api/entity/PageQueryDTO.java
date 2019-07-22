dcc177ae816c31f843b11894fecfc3e87531a397
leezhen <2371601496@qq,com>
2019-07-22 10:38:14 +0800

    修改测试问题1--分页

--- tzh-api/src/main/java/ops/school/api/entity/PageQueryDTO.java
+++ tzh-api/src/main/java/ops/school/api/entity/PageQueryDTO.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class PageQueryDTO implements Serializable {
 
-
+    private static final long serialVersionUID = 1L;
 
     private Integer page;
 
@@ -17,4 +17,52 @@ public class PageQueryDTO implements Serializable {
     private String query;
 
     private String queryType;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getQueryType() {
+        return queryType;
+    }
+
+    public void setQueryType(String queryType) {
+        this.queryType = queryType;
+    }
 }
