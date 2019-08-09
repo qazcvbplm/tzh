b899fc91a929778305449e9cbf0beca2aed405bd
leezhen <2371601496@qq,com>
2019-08-09 12:10:57 +0800

    1100

--- tzh/src/main/java/ops/school/config/MyInterceptor.java
+++ tzh/src/main/java/ops/school/config/MyInterceptor.java
@@ -61,7 +61,7 @@ public class MyInterceptor implements HandlerInterceptor {
         }
         Long endTime = System.currentTimeMillis();
         System.out.println("拦截器pre处理时间(ms)：" + (endTime - startTime));
-        new ResultUtil().error(request, response, PublicErrorEnums.LOGIN_TIME_OUT.getErrorMessage());
+        new ResultUtil().error(request, response, "1100");
         return false;
     }
 
