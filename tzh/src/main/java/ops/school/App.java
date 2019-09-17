eb5c34cceccb86f2e3c47fa87a6938de6057e79b
qindaofang <qindaofang@zallds.com>
2019-09-17 16:49:39 +0800

    启动错误

--- tzh/src/main/java/ops/school/App.java
+++ tzh/src/main/java/ops/school/App.java
@@ -7,8 +7,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
